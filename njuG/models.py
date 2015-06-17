d703059d178f587aeb02be505f46483d8e65e1b3
huangtao800 <huangtao7725@live.com>
2015-06-17 14:57:15 +0800

    Add Comment and Post models

--- njuG/models.py
+++ njuG/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Comment(models.Model):
+    content = models.CharField(max_length=500)
+    user = models.ForeignKey(User)
+    post = models.ForeignKey(Post)
+    time = models.TimeField(auto_now=True)
+    replyFrom = models.ForeignKey(self, blank=True)
+    
+class Post(models.Model):
+    content = models.CharField(max_length=300)
+    user = models.ForeignKey(User)
+    time = models.TimeField(auto_now=True)
+    
