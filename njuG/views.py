a4e069ffcf807eb0e78df070265918ccca9af20b
huangtao800 <huangtao7725@live.com>
2015-06-18 21:50:39 +0800

    implete post method

--- njuG/views.py
+++ njuG/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from njuG.models import Post
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
 	return render(request, 'njuG/index.html');
 
+
 def post(request):
 	from django.utils import timezone
-	if(request.POST):
-		content = request.POST['content'];
-		post = Post(user=request.user, content=content, time=timezone().now)
+	if(request.method == 'POST'):
+		try:
+# 			content = request.POST['content'];
+# 			post = Post(user=request.user, content=content, time=timezone().now)
+# 			post.save()
+			responseDict = {'result':'success', 'msg':''}
+			return JsonResponse(responseDict)
+		except Exception as e:
+			responseDict = {'result':'false','msg': e.message}
+			return JsonResponse(responseDict)
+		
