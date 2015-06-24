7ec611bb390d6228a2d2a6e0fb7c377c51a9d110
huangtao800 <huangtao7725@live.com>
2015-06-24 11:44:11 +0800

    Implement like

--- njuG/urls.py
+++ njuG/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^post', views.post, name='post'),
+    url(r'^likePost', views.likePost, name='likePost'),
+    url(r'^notLikePost', views.notLikePost, name='notLikePost'),
 ]
--- njuG/views.py
+++ njuG/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from njuG.models import Post
+from njuG.models import Post, Like
 from django.http import JsonResponse
 
 # Create your views here.
@@ -29,4 +29,40 @@ def post(request):
 			return JsonResponse(responseDict)
 	else:
 		return JsonResponse({'result':0,'msg':'user not login'})
-		
+	
+def likePost(request):
+	if(request.method=='POST' and request.user.is_authenticated()):
+		try:
+			postid = request.POST['postid']
+			post = Post.objects.get(pk=postid)
+			user = request.user
+			like = Like(user=user, post = post)
+			like.save()
+			post.likes = post.likes+1;
+			post.save();
+			responseDict = {'result':1, 'msg': 1}
+			return JsonResponse(responseDict)
+		except Exception as e:
+			print e.message
+			responseDict = {'result':0, 'msg':e.message}
+			return JsonResponse(responseDict)
+	else:
+		return JsonResonse({'result':0, 'msg':'user not login'})
+	
+def notLikePost(request):
+	if(request.method=='POST' and request.user.is_authenticated()):
+		try:
+			postid = request.POST['postid']
+			post = Post.objects.get(pk=postid)
+			like = Like.objects.get(user=request.user, post=post)
+			like.delete()
+			post.likes = post.likes-1;
+			post.save();
+			responseDict = {'result':1, 'msg':''}
+			return JsonResponse(responseDict)
+		except Exception as e:
+			print e.message
+			responseDict = {'result':0, 'msg':e.message}
+			return JsonResponse(responseDict)
+	else:
+		return JsonResonse({'result':0, 'msg':'user not login'})
