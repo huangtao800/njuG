d82ff279b5b32b5877c73fee9ae6d331329774bc
huangtao800 <huangtao800@gmail.com>
2015-07-19 14:47:10 +0800

    Move showMessage() to common.js

--- static/js/createEvent.js
+++ static/js/createEvent.js
@@ -104,22 +104,3 @@ $(document).ready(function(){
 		}
 	});
 });
-
-function showMessage(success, content){
-	if(success==1){
-		var infoDiv = $("#postSuccess");
-		if(!infoDiv.length){
-			infoDiv = $(document.createElement("div"));
-			infoDiv.addClass("ui positive message message");
-			var p = $(document.createElement("p"));
-			p.append(content);
-			infoDiv.append(p);
-			infoDiv.hide();
-			infoDiv.appendTo("body");
-		}
-		infoDiv.transition('fade');
-		setTimeout(function(){
-			infoDiv.transition('fade');
-		},2000);
-	}
-}
