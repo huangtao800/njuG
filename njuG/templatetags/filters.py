482732f11eaa8dc82821f229c0d1865d186082c8
huangtao800 <huangtao800@gmail.com>
2015-07-21 19:54:15 +0800

    Use time-ascending order to sort grouped blogComments

--- njuG/templatetags/filters.py
+++ njuG/templatetags/filters.py
@@ -5,3 +5,8 @@ register = template.Library()
 def addCSS(field, css):
     """Add CSS class to widget"""
     return field.as_widget(attrs={"class": css})
+
+@register.filter_function
+def order_by(queryset, args):
+    args = [x.strip() for x in args.split(',')]
+    return queryset.order_by(*args)
