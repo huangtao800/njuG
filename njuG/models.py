9cf559be5861c6082d02a6ce696f23ff99a56b48
huangtao800 <huangtao800@gmail.com>
2015-07-25 14:57:32 +0800

    Automatically delete file on model deletion

--- njuG/models.py
+++ njuG/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import pre_delete
+from django.dispatch.dispatcher import receiver
 
 # Create your models here.
 
@@ -62,6 +64,12 @@ class Picture(models.Model):
 class Image(models.Model):
     file = models.ImageField(upload_to="images")
     user = models.ForeignKey(User)
+
+@receiver(pre_delete, sender=Image)
+def image_delete(sender, instance, **kwargs):
+    # Pass false so FileField doesn't save the model.
+    if(instance.file):
+        instance.file.delete(False)
         
         
 class Blog(Event):
