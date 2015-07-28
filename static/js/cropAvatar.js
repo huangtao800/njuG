4d195d408efa6054c66258b89ffcf43e020f0c0b
huangtao800 <huangtao800@gmail.com>
2015-07-29 00:14:38 +0800

    Fix bug: cropper not working when choosing no file

--- static/js/cropAvatar.js
+++ static/js/cropAvatar.js
@@ -1,3 +1,22 @@
+function removeCropperContainer(){
+	var cropper = $(".cropper-container");
+	if(cropper.length>0){
+		cropper.remove();
+	}
+}
+
+function removeOldImg(){
+	var img = $("#cropImg");
+	if(img.length>0){
+		img.remove();
+	}
+}
+
+function createNewImg(){
+	var newImg = $("<img />").attr("id", "cropImg");
+	$(".cropContainer").append(newImg);
+}
+
 function readImage(input) {
     if ( input.files && input.files[0] ) {
         var FR= new FileReader();
@@ -23,23 +42,26 @@ function cropImage(){
 
 $(document).ready(function(){
     $("#avatarInput").change(function(){
-    	var cropper = $(".cropper-container");
-    	if(cropper.length>0){
-    		cropper.remove();
-    		var img = $("#cropImg");
-    		img.remove();
-    		var newImg = $("<img />").attr("id", "cropImg");
-    		$(".cropContainer").append(newImg);
-    	}
-        readImage( this );
-		setTimeout(function(){
-		  cropImage();
-		}, 100);
-		// setTimeout(function(){
-			// $(".cropper-container").attr("width", "400px").attr("height", "400px");
-		// },100);
-		$(".cropper-container").width("450px").height("400px");
-		
+		removeCropperContainer();
+		removeOldImg();
+		createNewImg();
+		if(this.files && this.files[0]){
+	        readImage( this );
+			setTimeout(function(){
+			  cropImage();
+			}, 50);
+			$(".cropper-container").width("450px").height("400px");
+			$("#cropBtn").prop("disabled",false);
+			$("#uploadAvatarBtn").prop("disabled",false);
+		}else{
+			$("#cropBtn").prop("disabled",true);
+			$("#uploadAvatarBtn").prop("disabled",true);
+		}
+
     });
-    // cropImage();
+    
+    $("#cropBtn").click(function(){
+    	var data = $("#cropImg").cropper("getData");
+    	alert(data);
+    });
 });
