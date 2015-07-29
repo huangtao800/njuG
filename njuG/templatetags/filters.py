6e6fed72010b13b7fbf1d333b7d4bfb30262b014
huangtao800 <huangtao800@gmail.com>
2015-07-29 17:12:26 +0800

    Show avatars on index.html

--- njuG/templatetags/filters.py
+++ njuG/templatetags/filters.py
@@ -11,3 +11,10 @@ def addCSS(field, css):
 def order_by(queryset, args):
     args = [x.strip() for x in args.split(',')]
     return queryset.order_by(*args)
+
+@register.filter_function
+def getAvatarPath(username):
+    if(username):
+        return "/static/img/avatar/" + username
+    else:
+        return "/static/img/avatar/Default-Avatar.jpg"
