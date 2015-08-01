2133892a02ec7a1417dc8135f03c9bd2f64d26ce
huangtao8000 <huangtao800@gmail.com>
2015-08-01 04:36:53 +0000

    Merge files

--- njuGay/urls.py
+++ njuGay/urls.py
@@ -19,6 +19,7 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', include('njuG.urls')),
