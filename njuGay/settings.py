8b03469b949d9db182e2e810e2d14693f827cf68
huangtao800 <huangtao800@gmail.com>
2016-01-08 21:37:39 -0800

    Do not load emoji css and js files for mobile device

--- njuGay/settings.py
+++ njuGay/settings.py
@@ -57,6 +57,9 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+
+	# detect mobile device
+	'mobileesp.middleware.MobileDetectionMiddleware',
 )
 
 ROOT_URLCONF = 'njuGay.urls'
